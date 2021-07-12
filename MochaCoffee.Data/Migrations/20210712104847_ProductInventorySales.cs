c2aa44200fccfbe457704c3e13eb3e470ee0de41
kavisonsugumaran <kavison4@gmail.com>
2021-07-12 16:19:45 +0530

    Add entity models

---
+++ MochaCoffee.Data/Migrations/20210712104847_ProductInventorySales.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MochaCoffee.Data.Migrations
+{
+    public partial class ProductInventorySales : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
