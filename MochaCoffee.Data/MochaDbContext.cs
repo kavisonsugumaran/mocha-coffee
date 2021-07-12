f4f7448f68bf1800b82df5d6ffc6f0a49576f885
kavisonsugumaran <kavison4@gmail.com>
2021-07-12 13:22:26 +0530

    Initial Commit

---
+++ MochaCoffee.Data/MochaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MochaCoffee.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MochaCoffee.Data
+{
+    public class MochaDbContext : IdentityDbContext
+    {
+        public MochaDbContext() { }
+        public MochaDbContext(DbContextOptions options) : base(options) { }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductInventory> ProductInventories { get; set; }
+        public virtual DbSet<ProductInventorySnapshot> ProductInventorySnapshots { get; set; }
+        public virtual DbSet<SalesOrder> SalesOrders { get; set; }
+        public virtual DbSet<SalesOrderItem> SalesOrderItems { get; set; }
+    }
+}
